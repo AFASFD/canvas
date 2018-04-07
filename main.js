6356ace4b62f714abdf91bccac23d4509f98b171
FrankYang <1025414646@qq.com>
2018-04-07 17:39:41 +0800

    修改

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-let self = this;
-document.addEventListener('touchstart', function (e) {
-    self.moveY = e.targetTouches[0].pageY;
-})
-document.addEventListener('touchmove', function (e){
-    e.preventDefault();
-    let moveWidth = self.moveY - e.targetTouches[0].pageY;
-    if (moveWidth !== 0) {
-        document.body.scrollTop += moveWidth;
-    }
-})
-document.addEventListener('touchmove', function (e) {
-    e.preventDefault();
-})
+
 //获取画布
 var canvas = document.getElementById('canvas')
 var content = canvas.getContext('2d')
